fb51b1291af0877f1d105b63208cacb41eea8e12
Analia Mok <analiam319@gmail.com>
2019-12-30 11:45:27 -0500

    Start color customization

--- wp-content/themes/surfschool/index.php
+++ wp-content/themes/surfschool/index.php
@@ -12,7 +12,7 @@ get_header();
 ?>
 
 <main id="site-content">
-	<h1 class="font-sans font-bold">Surf School Theme</h1>
+	<h1 class="font-sans font-bold text-teal-900">Surf School Theme</h1>
 </main>
 
 <?php
--- wp-content/themes/surfschool/tailwind.config.js
+++ wp-content/themes/surfschool/tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        teal: {
+          100: '#F4FCFC',
+          200: '#D3F6F8',
+          300: '#A9EBF0',
+          400: '#81DFE6',
+          500: '#34C3CD',
+          600: '#27ABB4',
+          700: '#1B9199',
+          800: '#12757C',
+          900: '#052D30',
+        },
+      },
       fontFamily: {
         sans: [
           'Inter',
