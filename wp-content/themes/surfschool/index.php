1f215ba3f42700cc9f4aee6a72eee2a22bca60a2
Analia Mok <analiam319@gmail.com>
2020-01-01 10:35:39 -0500

    Implemented data massaging

--- wp-content/themes/surfschool/index.php
+++ wp-content/themes/surfschool/index.php
@@ -17,10 +17,26 @@ $hero_title = get_field('hero_title');
 $hero_img = get_field('hero_image');
 $hero_img_url = $hero_img['url'] ?? '';
 
-// TODO: Tabs Massaging
+// Tabs Massaging
+$tab_nums = ['one', 'two', 'three', 'four'];
+$tab_content = [];
+$tab_details = [];
 
+foreach ($tab_nums as $num) {
+	$tab_content[] = [
+		'icon'        => get_field('level_' . $num . '_icon'),
+		'title'       => get_field('level_' . $num . '_title'),
+		'description' => get_field('level_' . $num . '_tab_description'),
+	];
+
+	$tab_details[] = [
+		'image'       => get_field('level_' . $num . '_detail_image'),
+		'title'       => get_field('level_' . $num . '_detail_title'),
+		'description' => get_field('level_' . $num . '_detail_description'),
+		'link'        => get_field('level_' . $num . '_link'),
+	];
+}
 ?>
-
 
 <header id="site-header" class="min-w-full h-screen-90 max-h-screen bg-center bg-cover bg-no-repeat px-8 py-12" style="background-image: url('<?php echo $hero_img_url; ?>');" role="banner">
 	<div class="container mx-auto h-full flex items-center">
@@ -34,8 +50,7 @@ $hero_img_url = $hero_img['url'] ?? '';
 </header><!-- #site-header -->
 
 <main id="site-content">
-	<!-- TODO: Implement tabs -->
-	<h1 class="font-sans font-bold text-teal-900">Surf School Theme</h1>
+
 </main>
 
 <?php
